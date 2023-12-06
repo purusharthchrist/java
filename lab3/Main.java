b74626359e836a1d0437a825c2a13a3363987663
purusharthchrist <purusharth.malik@msam.christuniversity.in>
2023-12-06 18:40:17 +0530

    added comments to Main.java

--- lab3/Main.java
+++ lab3/Main.java
@@ -1,9 +1,14 @@
+//importing the packages
 import java.util.*;
 
+//base class
 class Employee{
     int emp_id;
     String emp_name;
     String emp_desig;
+    double bonus;
+
+    //class constructor
     Employee(int e_id, String e_name, String desig){
         this.emp_id = e_id;
         this.emp_name = e_name;
@@ -11,6 +16,7 @@ class Employee{
         System.out.println("Employee Created!");
     }
 
+    //function to update employee fields
     void setter(int val){
         Scanner sc = new Scanner(System.in);
         switch (val){
@@ -33,9 +39,10 @@ class Employee{
         System.out.println("The new employee details are");
         System.out.println("Employee ID: "+this.emp_id);
         System.out.println("Employee Name: "+this.emp_name);
-        System.out.println("EMployee Designation: "+this.emp_desig);
+        System.out.println("Employee Designation: "+this.emp_desig);
     }
 
+    //function to display the employee details
     void display(){
         System.out.println("The employee details are");
         System.out.println("Employee ID: "+this.emp_id);
@@ -43,52 +50,96 @@ class Employee{
         System.out.println("Employee Designation: "+this.emp_desig);
     }
 
-    double calculateBonus(){
-        
+    //function to calculate bonus of all the employees
+    void calculateBonus(double sal){
+        this.bonus = sal*0.25;
     }
 }
-
 
 
 class HourlyEmployee extends Employee{
     double hourlyRate;
     int hoursWorked;
+    
+    //constructor
     HourlyEmployee(int e_id, String e_name, String desig, double h_rate, int h_work){
+        //using the constructor of the parent class
         super(e_id, e_name, desig);
         this.hourlyRate = h_rate;
         this.hoursWorked = h_work;
     }
 
+    //function to calculate weekly salary
     double weekly_sal(){
         return hourlyRate*hoursWorked;
     }
 
+    //overriding the calculateBonus method of the parent class
+    void calculateBonus(){
+        //using the base class function
+        super.calculateBonus(this.weekly_sal());
+        this.bonus = this.bonus + 1000;
+    }
+
+    //overriding the display function
     void display(){
         super.display();
         System.out.println("Weekly salary is: "+this.weekly_sal());
+        this.calculateBonus();
+        System.out.println("Bonus salary(per month) is: "+this.bonus);
     }
 }
 
 class SalariedEmployee extends Employee{
     double monthlySalary;
+    //constructor
     SalariedEmployee(int e_id, String e_name, String desig, double m_sal){
+        //using the constructor of the parent class
         super(e_id, e_name, desig);
         this.monthlySalary = m_sal;
     }
 
+    //overriding the weekly_sal function of the base class
     double weekly_sal(){
         return monthlySalary/4;
     }
 
+    //overriding the calculateBonus function of the base class
+    void calculateBonus(){
+        //using the base class function
+        super.calculateBonus(this.weekly_sal());
+        this.bonus = this.bonus + 2000;
+    }
+
+    //overiding the display function of the base class
     void display(){
         super.display();
         System.out.println("Weekly salary is: "+this.weekly_sal());
+        this.calculateBonus();
+        System.out.println("Bonus salary(per month) is: "+this.bonus);
     }
 }
 
+class ExecutiveEmployee extends SalariedEmployee{
+    double bonusPercentage;
+
+    //constructor
+    ExecutiveEmployee(int e_id, String e_name, String desig, double m_sal, double bonus_p){
+        //using the constructor of the base class 
+        super(e_id, e_name, desig, m_sal);
+        this.bonusPercentage = bonus_p;
+    }
+
+    //overriding the calculateBonus function of the base class
+    void calculateBonus(){
+        this.bonus = (this.monthlySalary*bonusPercentage)/100;
+    }
+}
+
+//main function
 public class Main{
     public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
+    //instantiating the variables
     int id;
     String name;
     String designation;
@@ -96,6 +147,11 @@ public class Main{
     int sal_st;
     double sal;
     Employee new_emp;
+    int emp_type;
+    double bp;
+    int hwork;
+    //variable to take user input
+    Scanner sc = new Scanner(System.in);
 
     System.out.println("Enter the employee ID: ");
     id = sc.nextInt();
@@ -112,7 +168,6 @@ public class Main{
     sc.nextLine();
 
     if (sal_st==1){
-        int hwork;
         System.out.println("Enter the hourly salary: ");
         sal = sc.nextDouble();
         System.out.println("Enter the work hours: ");
@@ -120,15 +175,29 @@ public class Main{
         new_emp = new HourlyEmployee(id, name, designation, sal, hwork);
     }
     else{
-        System.out.println("Enter the monthly salary");
-        sal = sc.nextDouble();
-        new_emp = new SalariedEmployee(id, name, designation, sal);
+        System.out.println("Is this a regular empolyee or an executive employee?");
+        System.out.println("1 - Regular");
+        System.out.println("2 - Executive");
+        emp_type = sc.nextInt();
+        if (emp_type == 1){
+            System.out.println("Enter the monthly salary: ");
+            sal = sc.nextDouble();
+            new_emp = new SalariedEmployee(id, name, designation, sal);
+        }
+        else{
+            System.out.println("Enter the monthly salary: ");
+            sal = sc.nextDouble();
+            System.out.println("Enter the bonus percentage: ");
+            bp = sc.nextDouble();
+            new_emp = new ExecutiveEmployee(id, name, designation, sal, bp);
+        }
     }
     
 
     System.out.println("Do you want to edit any details? (1-Yes, 2-No)");
     end = sc.nextInt();
 
+    //the loop will be executed till the user wants to edit employee details
     while(end == 1){
             int change;
             System.out.println("Which field do you want to edit?");
@@ -144,7 +213,7 @@ public class Main{
             end = sc.nextInt();
         }
 
-    System.out.println("Details of the employee: ");
+    //displaying the employee details
     new_emp.display();
     }
 }
