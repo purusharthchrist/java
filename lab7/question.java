1e3513e865c36e757738730bdca281e28084a448
purusharthchrist <purusharth.malik@msam.christuniversity.in>
2024-01-12 00:46:54 +0530

    updated lab7

--- lab7/question.java
+++ lab7/question.java
@@ -5,6 +5,41 @@ class Customer{
     static HashMap<Integer, ArrayList<String>> customers = new HashMap<>();
     static int customerNo;
 
+    //Default customers
+    Customer(){
+        customerNo += 1;
+        ArrayList<String> cstDetails = new ArrayList<>();
+        cstDetails.add("Khushi");
+        cstDetails.add("9876589545");
+        cstDetails.add("khushi@gmail.com");
+        cstDetails.add("New Street");
+        customers.put(customerNo, cstDetails);
+
+        customerNo += 1;
+        cstDetails = new ArrayList<>();
+        cstDetails.add("Mishi");
+        cstDetails.add("8957625412");
+        cstDetails.add("mishi@gmail.com");
+        cstDetails.add("Old Street");
+        customers.put(customerNo, cstDetails);
+
+        customerNo += 1;
+        cstDetails = new ArrayList<>();
+        cstDetails.add("Suyash");
+        cstDetails.add("7856954856");
+        cstDetails.add("suyash@gmail.com");
+        cstDetails.add("East Street");
+        customers.put(customerNo, cstDetails);
+
+        customerNo += 1;
+        cstDetails = new ArrayList<>();
+        cstDetails.add("Vishwas");
+        cstDetails.add("7859645823");
+        cstDetails.add("vishwas@gmail.com");
+        cstDetails.add("West Street");
+        customers.put(customerNo, cstDetails);
+    }
+
     //constructor for customer registration
     Customer(String cstName, String phNo, String cstMail, String cstAddress){
         //incrementing customer number
@@ -61,7 +96,9 @@ class Customer{
     //display customer details
     static void display(){
         System.out.println("\nCustomer Details\n---------------------");
-        for(Object obj: customers.keySet()){
+        TreeSet<Integer> sortedCustomers = new TreeSet<>();
+        sortedCustomers.addAll(customers.keySet());
+        for(Object obj: sortedCustomers){
             System.out.println(obj+" : "+ customers.get(obj));
         }
     }
@@ -75,6 +112,40 @@ class Product{
     //hashmap for inventory
     static HashMap<Integer, Integer> inventory = new HashMap<>();
     //creating default product catalog
+    Product(){
+        ArrayList<String> temp = new ArrayList<>();
+        productId+=1;
+        temp.add("Milk");
+        temp.add("24");
+        temp.add("250");
+        catalog.put(productId, temp);
+        inventory.put(productId, 12);
+
+        temp = new ArrayList<>();
+        productId+=1;
+        temp.add("Bread");
+        temp.add("40");
+        temp.add("200");
+        catalog.put(productId, temp);
+        inventory.put(productId, 15);
+
+        temp = new ArrayList<>();
+        productId+=1;
+        temp.add("Butter");
+        temp.add("25");
+        temp.add("100");
+        catalog.put(productId, temp);
+        inventory.put(productId, 18);
+
+        temp = new ArrayList<>();
+        productId+=1;
+        temp.add("Eggs");
+        temp.add("12");
+        temp.add("20");
+        catalog.put(productId, temp);
+        inventory.put(productId, 30);
+    }
+    //creating products with user inputs
     Product(String productName, String price, String itemWeight, int quantity){
         //arraylist to store product details
         ArrayList<String> temp = new ArrayList<>();
@@ -133,13 +204,15 @@ class Product{
 
     //display product details
     static void displayProduct(){
-        for(Object obj: catalog.keySet()){
+        TreeSet<Integer> sortedOrders = new TreeSet<>();
+        sortedOrders.addAll(catalog.keySet());
+        for(Object obj: sortedOrders){
             System.out.println(obj+" : "+catalog.get(obj));
         }
     }
 
     //display inventory
-    void displayInventory(){
+    static void displayInventory(){
         for(Object obj: inventory.keySet()){
             System.out.println(obj+" : "+inventory.get(obj));
         }
@@ -153,13 +226,24 @@ class Order{
     //hashmap to store order details
     static HashMap<Integer, HashMap<Integer, Integer>> orderDetails = new HashMap<>();
     //hashmap to store the status of the order
-    HashMap<Integer, String> orderStatus = new HashMap<>();
+    static HashMap<Integer, String> orderStatus = new HashMap<>();
     //hashmap for items ordered by customers
-    HashMap<Integer, HashSet<String>> customerItems = new HashMap<>();
+    static HashMap<Integer, HashSet<String>> customerItems = new HashMap<>();
+    //hashmap for customerId and orderId
+    static HashMap<Integer, ArrayList<Integer>> customerOrders = new HashMap<>();
     //placing an order
     Order(int custId, HashMap<Integer, Integer> items){
         //incrementing the orderId
         orderId+=1;
+        //adding the order to the customerId
+        if (customerOrders.containsKey(custId)){
+            customerOrders.get(custId).add(orderId);
+        }
+        else{
+            ArrayList<Integer> tempArray = new ArrayList<>();
+            tempArray.add(orderId);
+            customerOrders.put(custId, tempArray);
+        }
         //updating the inventory
         for(Integer obj: items.keySet()){
             int prevQuant = Product.inventory.get(obj);
@@ -179,28 +263,44 @@ class Order{
                 Product.inventory.put(obj, prevQuant-items.get(obj));
 
                 //adding the item to customer items
-                customerItems.get(custId).add(Product.catalog.get(101).get(0));
+                if (customerItems.containsKey(custId))
+                    customerItems.get(custId).add(Product.catalog.get(obj).get(0));
+                else{
+                    //hashset to store the unique items
+                    HashSet<String> uniqueItems = new HashSet<>();
+                    uniqueItems.add(Product.catalog.get(obj).get(0));
+                    customerItems.put(custId, uniqueItems);
+                }
             }
             //updating the status
             orderStatus.put(orderId, "processed");
         }
         //adding the order details to the hashmap
         orderDetails.put(orderId, items);
+        System.out.println("Your order ID is: "+orderId);
     }
 
-    //display order details
+    //display order details for admin
     static void display(){
         for(Object obj: orderDetails.keySet()){
-            System.out.print(obj+" : ");
+            System.out.println("Order No.: "+obj);
             for(Object item: orderDetails.get(obj).keySet()){
                 //printing the order details
-                System.out.println(item+" : "+Product.catalog.get(item));
+                System.out.println(item+" : "+Product.catalog.get(item).get(0));
             }
         }
     }
 
+    //display order details for customer
+    static void display(int orderId){
+            for(Object item: orderDetails.get(orderId).keySet()){
+                //printing the order details
+                System.out.println(item+" : "+Product.catalog.get(item));
+            }
+    }
+
     //tracking the order
-    void trackOrder(int trackId){
+    static void trackOrder(int trackId){
         System.out.println("The order has been "+orderStatus.get(trackId));
     }
 }
@@ -211,8 +311,13 @@ public class question{
         Scanner sc = new Scanner(System.in);
         int outerFlag=1;
         int choice;
+
+        //initializing the customers and products
+        new Customer();
+        new Product();
+
         while (outerFlag==1){
-            System.out.println("Logging in as...\n1. Admin\n2. Customer\n3. Exit");
+            System.out.println("\n\nLogging in as...\n1. Admin\n2. Customer\n3. Exit");
             int temp = sc.nextInt();
             int flag=1;
                 switch (temp) {
@@ -221,7 +326,8 @@ public class question{
                     case 1:
                         while(flag==1){
                             System.out.println("\nWhat operation do you want to perform?");
-                            System.out.println("1. Add a new product\n2. Update details of an existing product\n3. View customer details\n4. View current orders\n5. Exit");
+                            System.out.println("1. Add a new product\n2. Update details of an existing product\n3. View sorted customer details");
+                            System.out.println("4. View current orders\n5. View sorted catalog\n6. Items ordered by different customers\n7.Exit");
                             choice = sc.nextInt();
 
                             switch (choice) {
@@ -271,8 +377,24 @@ public class question{
                                     Order.display();
                                     break;
 
-                                //exit
+                                //viewing the product catalog and the inventory
                                 case 5:
+                                    System.out.println("Product Catalog | Quantity");
+                                        System.out.println("--------------------------------");
+                                        for(Object obj: Product.catalog.keySet()){
+                                            System.out.println(obj+" : "+Product.catalog.get(obj)+" | "+Product.inventory.get(obj));
+                                        }
+                                    break;
+
+                                //viewing items ordered by the customer
+                                case 6:
+                                    System.out.println("\n\nItems ordered by the customers");
+                                    System.out.println("---------------------------------");
+                                    for(Object obj: Order.customerItems.keySet()){
+                                        System.out.println(obj+" : "+Order.customerItems.get(obj));
+                                    }
+                                //exit
+                                case 7:
                                     flag=0;
                                     break;
                             }
@@ -282,7 +404,7 @@ public class question{
                     //customer functionality
                     case 2:
                         while(flag==1){
-                            System.out.println("What operation do you want to perform?");
+                            System.out.println("\nWhat operation do you want to perform?");
                             System.out.println("1. Register\n2. Update details\n3. Place an order\n4. Track an order\n5. Exit");
                             temp = sc.nextInt();
                             switch (temp) {
@@ -315,11 +437,47 @@ public class question{
 
                                 //placing an order
                                 case 3:
-                                    
+                                    HashMap<Integer, Integer> items = new HashMap<>();
+                                    System.out.println("Please enter your login ID: ");
+                                    login = sc.nextInt();
+
+                                    //taking input
+                                    int moreItems = 1;
+                                    while(moreItems==1){
+                                        //printing out the products
+                                        System.out.println("Product Catalog | Quantity");
+                                        System.out.println("--------------------------------");
+                                        for(Object obj: Product.catalog.keySet()){
+                                            System.out.println(obj+" : "+Product.catalog.get(obj)+" | "+Product.inventory.get(obj));
+                                        }
+                                        System.out.println("Enter the product ID: ");
+                                        int prodId = sc.nextInt();
+                                        System.out.println("Enter the quantity");
+                                        int quantity = sc.nextInt();
+                                        items.put(prodId, quantity);
+                                        System.out.println("Item added!");
+                                        System.out.println("Do you want to order more items?");
+                                        System.out.println("1. Yes\n2. No");
+                                        int askUser = sc.nextInt();
+                                        if (askUser==2)
+                                            moreItems=0;
+                                    }
+                                    System.out.println("Order Placed!\n");
+                                    //storing order details
+                                    new Order(login, items);
                                     break;
 
+                                //tracking an order
                                 case 4:
-                                    
+                                    System.out.println("Please enter your login ID: ");
+                                    login = sc.nextInt();
+                                    //printing all orders placed by the customer
+                                    for(Integer obj: Order.customerOrders.get(login)){
+                                        System.out.println();
+                                        Order.display(obj);
+                                        System.out.println("\nStatus: ");
+                                        Order.trackOrder(obj);
+                                    }
                                     break;
 
                                 case 5:
